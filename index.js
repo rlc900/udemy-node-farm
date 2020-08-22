0776c50e8dc2bde77dec2a5e45e8016fcf7e1291
Rianna <rianna.cleary@gmail.com>
2020-08-22 15:52:47 -0400

    added file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs') // stands for 'file system'
 
 const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8")
-console.log(textIn)
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+
+fs.writeFileSync('./starter/txt/output.txt', textOut)
+console.log('File writen!')
