1fca42d13c61907d233d86a0067e7a90fa8a289a
Rianna <rianna.cleary@gmail.com>
2020-08-22 16:52:10 -0400

    implement async

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs') // stands for 'file system'
 
-const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8")
-console.log(textIn);
+// SYNCHRONOUS/BLOCKING
+// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8")
+// console.log(textIn);
+//
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+//
+// fs.writeFileSync('./starter/txt/output.txt', textOut)
+// console.log('File written!')
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// ASYNC/NON-BLOCKING
 
-fs.writeFileSync('./starter/txt/output.txt', textOut)
-console.log('File writen!')
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+
+  fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log('LINE 16', data2)
+    fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+      console.log('LINE 18', data3)
+
+      fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+        console.log('LINE 21', 'File has been written! 👻')
+      })
+    })
+  })
+})
+console.log('I will print first!')
